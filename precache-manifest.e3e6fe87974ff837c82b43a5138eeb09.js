1945094fa89662e2e8ace223f281b2a1ccab004c
Peter Neely <pgneely@gmail.com>
2019-09-21 12:30:19 -0700

    Updates

---
+++ precache-manifest.e3e6fe87974ff837c82b43a5138eeb09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4184802b442c34d68b79c8a65e1968a",
+    "url": "/launch/index.html"
+  },
+  {
+    "revision": "70aeaf8155b6e7d15da5",
+    "url": "/launch/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "95050a16b7db5947b937",
+    "url": "/launch/static/js/2.cf3f79e6.chunk.js"
+  },
+  {
+    "revision": "70aeaf8155b6e7d15da5",
+    "url": "/launch/static/js/main.86987ac1.chunk.js"
+  },
+  {
+    "revision": "b767c6225d9adb85b324",
+    "url": "/launch/static/js/runtime-main.720a3540.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/launch/static/media/logo.25bf045c.svg"
+  }
+]);
