01a92c13f3ec3997a8a1d93275562312bef25572
Peter Neely <peter.neely@sony.com>
2019-09-23 15:48:07 -0700

    conditionally load new tab page

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 const { tabs } = chrome;
 
 tabs.onCreated.addListener(tab => {
-    tabs.update(tab.id, { url: 'https://peterneely.github.io/launch/' });
+    if (tab.url === 'chrome://newtab/') {
+        tabs.update(tab.id, { url: 'https://peterneely.github.io/launch/' });
+    }
 });
