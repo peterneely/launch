47240e4b3ee618c7cf8060d703b5933aa429406d
Peter Neely <peter.neely@sony.com>
2019-09-24 11:17:15 -0700

    Style improvements. WIP Chrome extension

--- background.js
+++ background.js
@@ -1,7 +1,14 @@
-const { tabs } = chrome;
+// eslint-disable-next-line no-undef
+const { bookmarks, tabs } = chrome;
 
 tabs.onCreated.addListener(tab => {
     if (tab.url === 'chrome://newtab/') {
+        // bookmarks.getTree(results => {
+        //     console.log(results);
+        // });
+        bookmarks.getSubTree('6', results => {
+            console.log(results);
+        });
         tabs.update(tab.id, { url: 'https://peterneely.github.io/launch/' });
     }
 });
