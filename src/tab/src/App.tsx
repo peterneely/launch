7617237dfb736817af9ea88cc83760ba01ecaa61
Peter Neely <peter.neely@sony.com>
2019-10-13 20:02:20 -0700

    separate tab and popup apps

--- src/tab/src/App.tsx
+++ src/tab/src/App.tsx
@@ -3,13 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    window.chrome.runtime.sendMessage({ message: 'GET_BOOKMARKS' }, response => {
+      console.log(response.bookmarks);
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            POPUP: Edit <code>src/App.tsx</code> and save to reload.
+            TAB: Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
             className="App-link"
