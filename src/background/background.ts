130a65b914bf642946e63acd810dcb8bdfb0f7b0
Peter Neely <peter.neely@sony.com>
2019-10-13 19:34:18 -0700

    proof of concept fetching bookmarks

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,2 +1,13 @@
 const isThisBackground = true;
 console.log('isThisBackground', isThisBackground);
+
+chrome.runtime.onMessage.addListener(
+  (request, sender, sendResponse) => {
+    if (request.message === 'GET_BOOKMARKS') {
+      chrome.bookmarks.getTree(treeNode => {
+        console.log(treeNode);
+        sendResponse({ bookmarks: treeNode });
+      });
+      return true;
+    }
+  });
