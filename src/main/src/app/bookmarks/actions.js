06dda80fae446822c06b2642398930b93ec44f94
Peter Neely <pgneely@gmail.com>
2019-12-03 11:24:34 -0800

    refactor

--- src/main/src/app/bookmarks/actions.js
+++ src/main/src/app/bookmarks/actions.js
@@ -3,9 +3,14 @@ import { getBookmarks } from '../browser';
 import { getFolders } from './actionUtils';
 
 export const loadBookmarks = (folderId, savedSettings) => async dispatch => {
-  const bookmarks = await getBookmarks(folderId, savedSettings);
-  dispatch({ type: types.BOOKMARKS_LOAD_BOOKMARKS_SUCCESS, payload: { bookmarks, folderId } });
-  return bookmarks;
+  try {
+    const bookmarks = await getBookmarks(folderId, savedSettings);
+    dispatch({ type: types.BOOKMARKS_LOAD_BOOKMARKS_SUCCESS, payload: { bookmarks, folderId } });
+    return bookmarks;
+  } catch (error) {
+    dispatch({ type: types.BOOKMARKS_LOAD_BOOKMARKS_ERROR, payload: { error } });
+    return [];
+  }
 };
 
 export const loadFolders = () => async dispatch => {
--- src/main/src/app/bookmarks/reducer.js
+++ src/main/src/app/bookmarks/reducer.js
@@ -9,26 +9,41 @@ const initialState = {
   foldersLoaded: false, // whether folders have been loaded
 };
 
+const handleLoadBookmarksSuccess = (state, payload) => {
+  const { bookmarksByFolderId, errorsByKey } = state;
+  const { bookmarks, folderId } = payload;
+  return {
+    ...state,
+    bookmarksByFolderId: {
+      ...bookmarksByFolderId,
+      [folderId]: bookmarks,
+    },
+    errorsByKey: removePath({ object: errorsByKey, path: 'bookmarks' }),
+  };
+};
+
+const handleLoadFoldersSuccess = (state, payload) => {
+  const { errorsByKey } = state;
+  const { foldersById } = payload;
+  return {
+    ...state,
+    errorsByKey: removePath({ object: errorsByKey, path: 'folders' }),
+    foldersById,
+    foldersLoaded: true,
+  };
+};
+
 export const reducer = (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
+    case types.BOOKMARKS_LOAD_BOOKMARKS_ERROR:
+      return { ...state, errorsByKey: assignPath({ object: state.errorsByKey, path: 'bookmarks', value: payload.error }) };
     case types.BOOKMARKS_LOAD_BOOKMARKS_SUCCESS:
-      return {
-        ...state,
-        bookmarksByFolderId: {
-          ...state.bookmarksByFolderId,
-          [payload.folderId]: payload.bookmarks
-        }
-      }
+      return handleLoadBookmarksSuccess(state, payload);
     case types.BOOKMARKS_LOAD_FOLDERS_ERROR:
-      return { ...state, errorsByKey: assignPath({ object: state.errorsByKey, path: 'load', value: payload.error }) };
+      return { ...state, errorsByKey: assignPath({ object: state.errorsByKey, path: 'folders', value: payload.error }) };
     case types.BOOKMARKS_LOAD_FOLDERS_SUCCESS:
-      return {
-        ...state,
-        errorsByKey: removePath({ object: state.errorsByKey, path: 'load' }),
-        foldersById: payload.foldersById,
-        foldersLoaded: true,
-      };
+      return handleLoadFoldersSuccess(state, payload);
     case types.BOOKMARKS_SET_FOLDER:
       return { ...state, folder: payload.folder };
     default:
